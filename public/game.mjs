1ce76afef457026628e0d83b1e0b46e06552d220
leprecon73 <leprecon73@gmail.com>
2024-12-05 17:41:36 +0400

    12

--- public/game.mjs
+++ public/game.mjs
@@ -3,4 +3,9 @@ import Collectible from './Collectible.mjs';
 
 const socket = io();
 const canvas = document.getElementById('game-window');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+fdgd
+//ctx.clearRect(0, 0, canvas.width, canvas.height);
+console.log(canvas.width, canvas.height);
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
